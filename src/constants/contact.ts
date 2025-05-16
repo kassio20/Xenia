8bbeb94b36ba0f6eb83a14ef694b98e404d68fff
kassio20 <kassiosantos113@gmail.com>
2025-05-16 12:01:09 -0300

    Update WhatsApp phone number to (73)99832-3357

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,9 +1,9 @@
 export const CONTACT_INFO = {
-  phone: "73 99914-7925",
-  phoneFormatted: "(73) 99914-7925",
-  phoneLink: "+5573999147925",
+  phone: "73 99832-3357",
+  phoneFormatted: "(73) 99832-3357",
+  phoneLink: "+5573998323357",
   instagram: "https://www.instagram.com/imoveisestruturar",
-  whatsapp: "https://wa.me/5573999147925",
+  whatsapp: "https://wa.me/5573998323357",
   email: "vendas@imoveisestruturar.com.br",
   address: "Prado, Bahia - Brasil",
   businessHours: {
